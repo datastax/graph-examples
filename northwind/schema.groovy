a7592d5a813b827de440659d807b3a31025f3260
Jeremy Hanna <jeromatron@gmail.com>
2017-07-07 12:41:14 -0500

    Added game_index back to pokemon mapping and supplemental northwind data.

--- northwind/schema.groovy
+++ northwind/schema.groovy
@@ -1,44 +1,54 @@
 // Define the possible properties for any vertex or edge label
-schema.propertyKey('id').Int().single().create()
+schema.propertyKey('id').Text().create()
 
-schema.propertyKey('customerId').Text().single().create()
-schema.propertyKey('name').Text().single().create()
-schema.propertyKey('title').Text().single().create()
-schema.propertyKey('address').Text().single().create()
-schema.propertyKey('city').Text().single().create()
-schema.propertyKey('postalCode').Text().single().create()
-schema.propertyKey('phone').Text().single().create()
-schema.propertyKey('fax').Text().single().create()
-schema.propertyKey('company').Text().single().create()
+schema.propertyKey('customerId').Text().create()
+schema.propertyKey('name').Text().create()
+schema.propertyKey('title').Text().create()
+schema.propertyKey('address').Text().create()
+schema.propertyKey('city').Text().create()
+schema.propertyKey('postalCode').Text().create()
+schema.propertyKey('phone').Text().create()
+schema.propertyKey('fax').Text().create()
+schema.propertyKey('company').Text().create()
 
-schema.propertyKey('titleOfCourtesy').Text().single().create()
-schema.propertyKey('firstName').Text().single().create()
-schema.propertyKey('lastName').Text().single().create()
-schema.propertyKey('extension').Text().single().create()
-schema.propertyKey('homePhone').Text().single().create()
-schema.propertyKey('hireDate').Bigint().single().create()
-schema.propertyKey('notes').Text().single().create()
+schema.propertyKey('titleOfCourtesy').Text().create()
+schema.propertyKey('firstName').Text().create()
+schema.propertyKey('lastName').Text().create()
+schema.propertyKey('extension').Text().create()
+schema.propertyKey('homePhone').Text().create()
+schema.propertyKey('hireDate').Bigint().create()
+schema.propertyKey('notes').Text().create()
 
-schema.propertyKey('orderDate').Bigint().single().create()
-schema.propertyKey('shipName').Text().single().create()
-schema.propertyKey('shipAddress').Text().single().create()
-schema.propertyKey('shipCity').Text().single().create()
-schema.propertyKey('shipPostalCode').Text().single().create()
-schema.propertyKey('shippedDate').Bigint().single().create()
-schema.propertyKey('requiredDate').Bigint().single().create()
-schema.propertyKey('freight').Decimal().single().create()
+schema.propertyKey('orderDate').Bigint().create()
+schema.propertyKey('shipName').Text().create()
+schema.propertyKey('shipAddress').Text().create()
+schema.propertyKey('shipCity').Text().create()
+schema.propertyKey('shipPostalCode').Text().create()
+schema.propertyKey('shippedDate').Bigint().create()
+schema.propertyKey('requiredDate').Bigint().create()
+schema.propertyKey('freight').Decimal().create()
 
-schema.propertyKey('unitPrice').Decimal().single().create()
-schema.propertyKey('discount').Decimal().single().create()
-schema.propertyKey('quantity').Int().single().create()
+schema.propertyKey('unitPrice').Decimal().create()
+schema.propertyKey('discount').Decimal().create()
+schema.propertyKey('quantity').Int().create()
 
-schema.propertyKey('type').Text().single().create()
-schema.propertyKey('unitsInStock').Int().single().create()
-schema.propertyKey('unitsOnOrder').Int().single().create()
-schema.propertyKey('reorderLevel').Int().single().create()
-schema.propertyKey('discontinued').Boolean().single().create()
+schema.propertyKey('type').Text().create()
+schema.propertyKey('unitsInStock').Int().create()
+schema.propertyKey('unitsOnOrder').Int().create()
+schema.propertyKey('reorderLevel').Int().create()
+schema.propertyKey('discontinued').Boolean().create()
 
-schema.propertyKey('description').Text().single().create()
+schema.propertyKey('description').Text().create()
+
+schema.propertyKey('age').Int().create()
+
+schema.propertyKey('affinityScore').Int().create()
+
+schema.propertyKey('relationship').Text().create()
+
+schema.propertyKey('confidence').Int().create()
+
+schema.propertyKey('rating').Int().create()
 
 // Define the vertex labels with associated properties
 schema.vertexLabel('customer').partitionKey('id').properties('customerId', 'title', 'name', 'address', 'city', 'postalCode', 'phone', 'fax', 'company').create()
@@ -49,6 +59,7 @@ schema.vertexLabel('product').partitionKey('id').properties('name', 'type', 'uni
 schema.vertexLabel('category').partitionKey('id').properties('name', 'description').create()
 schema.vertexLabel('country').partitionKey('id').properties('name').create()
 schema.vertexLabel('region').partitionKey('id').properties('name').create()
+schema.vertexLabel('networkMember').partitionKey('id').properties('name', 'age').create()
 
 // Define the edge labels with cardinality and how they connect vertices
 schema.edgeLabel('sold').single().connection('employee', 'order').create()
@@ -59,10 +70,13 @@ schema.edgeLabel('livesInRegion').single().connection('customer', 'region').crea
 schema.edgeLabel('inCategory').single().connection('product', 'category').create()
 schema.edgeLabel('is').single().connection('item', 'product').create()
 schema.edgeLabel('reportsTo').single().connection('employee', 'employee').create()
+schema.edgeLabel('isNetworkMember').single().properties('confidence').connection('customer', 'networkMember').create()
+schema.edgeLabel('isFriendsWith').single().properties('affinityScore').connection('networkMember', 'networkMember').create()
+schema.edgeLabel('isRelatedTo').single().properties('relationship').connection('networkMember', 'networkMember').create()
+schema.edgeLabel('rated').single().properties('rating').connection('customer', 'product').create()
 
 // Add materialized views for what are essentially alternate keys to the data
 schema.vertexLabel('customer').index('byCustomerId').materialized().by('customerId').add()
-schema.vertexLabel('product').index('byName').materialized().by('name').add()
 schema.vertexLabel('category').index('byName').materialized().by('name').add()
 schema.vertexLabel('country').index('byName').materialized().by('name').add()
 schema.vertexLabel('region').index('byName').materialized().by('name').add()
@@ -70,3 +84,6 @@ schema.vertexLabel('region').index('byName').materialized().by('name').add()
 // Add a search index on product and customer name to be able to things like regex or fuzzy searching by name
 schema.vertexLabel('customer').index('search').search().by('name').add()
 schema.vertexLabel('product').index('search').search().by('name').add()
+
+// Add a search index on network member on name and age
+schema.vertexLabel('networkMember').index('search').search().by('name').by('age').add()
