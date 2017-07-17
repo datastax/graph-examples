8653892a5297d060af40f225b13a87e62fd4b33b
Jeremy Hanna <jeromatron@gmail.com>
2017-07-17 15:45:57 -0500

    Added edges to device vertices. Changed emailAddress to email.

--- fraud/schema.groovy
+++ fraud/schema.groovy
@@ -2,7 +2,7 @@
 schema.propertyKey('customerId').Uuid().ifNotExists().create()
 schema.propertyKey('firstName').Text().ifNotExists().create()
 schema.propertyKey('lastName').Text().ifNotExists().create()
-schema.propertyKey('emailAddress').Text().ifNotExists().create()
+schema.propertyKey('email').Text().ifNotExists().create()
 schema.propertyKey('streetAddress1').Text().ifNotExists().create()
 schema.propertyKey('streetAddress2').Text().ifNotExists().create()
 schema.propertyKey('city').Text().ifNotExists().create()
@@ -26,13 +26,18 @@ schema.propertyKey('amount').Decimal().ifNotExists().create()
 schema.propertyKey('chargebackNumber').Int().ifNotExists().create()
 schema.propertyKey('chargebackDate').Timestamp().ifNotExists().create()
 
+schema.propertyKey('type').Text().ifNotExists().create()
+schema.propertyKey('os').Text().ifNotExists().create()
+schema.propertyKey('osVersion').Text().ifNotExists().create()
+
 schema.propertyKey('relatedBy').Text().ifNotExists().create()
 
 // Vertex labels
-schema.vertexLabel('customer').partitionKey('customerId').properties('firstName', 'lastName', 'emailAddress', 'streetAddress1', 'streetAddress2', 'city', 'state', 'postalCode', 'countryCode', 'phoneNumber', 'created').ifNotExists().create()
+schema.vertexLabel('customer').partitionKey('customerId').properties('firstName', 'lastName', 'email', 'streetAddress1', 'streetAddress2', 'city', 'state', 'postalCode', 'countryCode', 'phoneNumber', 'created').ifNotExists().create()
 schema.vertexLabel('session').partitionKey('sessionId').properties('ipAddress', 'deviceId', 'sessionDate').ifNotExists().create()
 schema.vertexLabel('order').partitionKey('orderId').properties('orderDate', 'outcome', 'creditCardHashed', 'ipAddress', 'amount', 'deviceId').ifNotExists().create()
 schema.vertexLabel('chargeback').partitionKey('chargebackNumber').properties('chargebackDate', 'amount', 'creditCardHashed').ifNotExists().create()
+schema.vertexLabel('device').partitionKey('deviceId').properties('type', 'os', 'osVersion').ifNotExists().create()
 
 // Edge labels
 schema.edgeLabel('isRelatedTo').properties('relatedBy').connection('customer', 'customer').ifNotExists().create()
@@ -40,3 +45,4 @@ schema.edgeLabel('places').connection('customer', 'order').ifNotExists().create(
 schema.edgeLabel('logsInto').connection('customer', 'session').ifNotExists().create()
 schema.edgeLabel('chargedWith').connection('customer', 'chargeback').ifNotExists().create()
 schema.edgeLabel('resultsIn').connection('order', 'chargeback').ifNotExists().create()
+schema.edgeLabel('using').connection('session', 'device').connection('order', 'device').ifNotExists().create()
