18e455bd2d666bb7bccbcffcffe9aaea7d994d7d
Stephen Mallette <spmva@genoprime.com>
2018-01-29 07:22:51 -0500

    Cleanup formatting of .net DSL app.

--- killrvideo/dsl/dotnet/KillrVideo/App/App.cs
+++ killrvideo/dsl/dotnet/KillrVideo/App/App.cs
@@ -16,28 +16,24 @@ using static KillrVideo.Dsl.Enrichment;
 using static KillrVideo.Dsl.Recommender;
 using static KillrVideo.Dsl.__KillrVideo;
 
+using Vertex = Gremlin.Net.Structure.Vertex;
+
 namespace KillrVideo.App
 {
     public class App 
     {
         public static void Main() 
         {
-            /*
-            var graph = new Graph();
-            var killr = graph.Traversal().WithRemote(new DriverRemoteConnection(
-                new GremlinClient(new GremlinServer("localhost", 8182), mimeType:GremlinClient.GraphSON2MimeType), 
-                "killrvideo.g"));
-                */
-            IDseCluster cluster = DseCluster.Builder()
-                            .AddContactPoint("127.0.0.1")
-                            .WithGraphOptions(new GraphOptions().SetName("killrvideo"))
-                            .Build();
+            IDseCluster cluster = DseCluster.Builder().
+                                             AddContactPoint("127.0.0.1").
+                                             WithGraphOptions(new GraphOptions().SetName("killrvideo")).
+                                             Build();
             IDseSession session = cluster.Connect();
             var killr = DseGraph.Traversal(session);   
 
             PrintHeader("Actors for Young Guns", "killr.Movies(\"Young Guns\").Actors().Values(\"name\")");
             var results = killr.Movies("Young Guns").Actors().Values<string>("name").ToList();
-            results.ToList().ForEach(r => Console.WriteLine(r));
+            PrintItems(results);
             
             PrintHeader("Ratings Distribution by Age for Young Guns", "killr.Movies(\"Young Guns\").Ratings().DistributionForAges(18, 40)");
             IDictionary<string,long> ratingsByAge = killr.Movies("Young Guns").Ratings().DistributionForAges(18, 40).Next();
@@ -45,23 +41,26 @@ namespace KillrVideo.App
             Console.WriteLine($"[{pairs}]");
 
             PrintHeader("Failed Validation", "killr.Movies(\"Young Guns\").Ratings().DistributionForAges(17,40)");
-            try {
+            try 
+            {
                 killr.Movies("Young Guns").Ratings().DistributionForAges(17,40).Next();
-            } catch (ArgumentException ae) {
+            } 
+            catch (ArgumentException ae) 
+            {
                 Console.WriteLine($"Caught ArgumentException: {ae.Message}");
             }
 
             PrintHeader("Five Recommendations for u460", "killr.Users(\"u460\").Recommend(5, 7).Values(KEY_TITLE)");
             results = killr.Users("u460").Recommend(5, 7).Values<string>(KeyTitle).ToList();
-            results.ToList().ForEach(r => Console.WriteLine(r));
+            PrintItems(results);
 
             PrintHeader("Five Recommendations for u460 that are comedies", "killr.Users(\"u460\").Recommend(5, 7, Genre(COMEDY)).Values(KEY_TITLE)");
             results = killr.Users("u460").Recommend(5, 7, Genre(Comedy)).Values<string>(KeyTitle).ToList();
-            results.ToList().ForEach(r => Console.WriteLine(r));
+            PrintItems(results);
 
             PrintHeader("Five Recommendations for u460 that use larger actor sampling and are comedies", "killr.users(\"u460\").recommend(5, 7, genre(COMEDY)).values(KEY_TITLE)");
             results = killr.Users("u460").Recommend(5, 7, LargeSample, Genre(Comedy)).Values<string>(KeyTitle).ToList();
-            results.ToList().ForEach(r => Console.WriteLine(r));
+            PrintItems(results);
 
             PrintHeader("Include some additional graph statistics about Young Guns", "killr.movies(\"Young Guns\").enrich(IN_DEGREE, OUT_DEGREE)");
             IDictionary<object,object> enriched = killr.Movies("Young Guns").enrich(InDegree, OutDegree).Next();
@@ -70,14 +69,23 @@ namespace KillrVideo.App
 
             PrintHeader("Insert/update movie and a actors for that movie", "killr.movie(\"m100000\", \"Manos: The Hands of Fate\",...).actor(...)"); 
             killr.Movie("m100000", "Manos: The Hands of Fate", 1966, 70, "USA", "Sun City Films").
-                    Ensure<Gremlin.Net.Structure.Vertex,object,Gremlin.Net.Structure.Vertex>(__KillrVideo.Actor("p1000000", "Tom Neyman")).
-                    Ensure<Gremlin.Net.Structure.Vertex,object,Gremlin.Net.Structure.Vertex>(__KillrVideo.Actor("p1000001", "John Reynolds")).
-                    Ensure<Gremlin.Net.Structure.Vertex,object,Gremlin.Net.Structure.Vertex>(__KillrVideo.Actor("p1000002", "Diane Mahree")).
-                    Iterate();
+                  Ensure<Vertex,object,Vertex>(__KillrVideo.Actor("p1000000", "Tom Neyman")).
+                  Ensure<Vertex,object,Vertex>(__KillrVideo.Actor("p1000001", "John Reynolds")).
+                  Ensure<Vertex,object,Vertex>(__KillrVideo.Actor("p1000002", "Diane Mahree")).
+                  Iterate();
+            Console.WriteLine("Updated 'Manos: The Hands of Fate'");
 
             PrintHeader("Get the actors for the newly added movie", "killr.movies(\"Manos: The Hands of Fate\").actors().values(\"name\")");
             results = killr.Movies("Manos: The Hands of Fate").Actors().Values<String>("name").ToList();
-            results.ToList().ForEach(r => Console.WriteLine(r));
+            PrintItems(results);
+        }
+
+        private static void PrintItems(IEnumerable<object> results)
+        {
+            foreach (var r in results)
+            {
+                Console.WriteLine(r);
+            }
         }
 
         private static void PrintHeader(string title) 
